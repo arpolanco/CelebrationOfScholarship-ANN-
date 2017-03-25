5149283c7b48d75ae3c534653d3e76e0d1c12190
Austin Ferguson <Austin Ferguson>
2017-03-24 22:46:55 -0400

    Starting to match, but overfitting is a huge problem.

--- March18th/dataPreparation.py
+++ March18th/dataPreparation.py
@@ -3,6 +3,7 @@ import pandas_datareader.data as web   # Package and modules for importing data;
 import datetime
 import numpy as np
 
+numElements = 6
 
 def getDataForSymbol(sym, start, end):
     df = web.DataReader(sym, "yahoo", start, end) # returns a dataframe
@@ -17,7 +18,7 @@ def getDataForSymbolList(symList, start, end):
 
 def formatForTraining(data, start, end, daysPerSet):
     total = len(data) * (len(data[list(data.keys())[0]]) - daysPerSet - 1)
-    arr = np.zeros((total, 6 * daysPerSet))
+    arr = np.zeros((total, numElements * daysPerSet))
     outputArr = np.zeros((total, 1))
     curSet = 0
     for sym in data:
@@ -30,7 +31,7 @@ def formatForTraining(data, start, end, daysPerSet):
                 arr[curSet, idx+3] = data[sym]["Adj Close"][j]
                 arr[curSet, idx+4] = data[sym]["High"][j]
                 arr[curSet, idx+5] = data[sym]["Volume"][j]
-                idx+=6
+                idx+=numElements
             outputArr[curSet, 0] = data[sym]["Adj Close"][j+1]   
             curSet+=1
     return arr, outputArr
@@ -40,39 +41,41 @@ def createTrainingData(symList, start, end, daysPerSet):
     x, y = formatForTraining(data, start, end, daysPerSet)
     return x,y
 
-loadTest = False
 
-# We will look at stock prices over the past year, starting at January 1, 2016
-start = datetime.datetime(2016,1,1)
-end = datetime.date.today()
- 
-L = ["PIH","SHLM","ACTG","AKAO","ADMS","AAAP","AEHR","AGYS","ATSG","AMRI","ALJJ","ALQA","SWIN","AMBA","AETI","CRMT","IBUY","ANDAR","ABAC","AMAT","ARDM","APLP","ARTX","ASNA","ALOT","ACFC","ATRI","ADP","AHPAU","AZRX","BLDP","OZRK","BHACW","BBBY","BGCP","BLRX","BSTC","BBRY","BLBD","BNCN","BPFHP","BLIN","BRKS","CHRW","CLMS","CALA","CPLA","CAPN","CSII","CRTN","CBIO","CDW","CLLS","CSFL","CRNT","CFCOU","CHKP","CMRX","CNIT","IMOS","CMCT","CZFC","CLRO","CNV","CGNX","COLB","CHUBK","CGEN","CFMS","CFRX","CORT","COUP","CRAY","XRDC","CTIC","CVV","CONE","DRIOW","DWSN","DENN","DXTR","DRAD","DISH","DORM","DXPE","EWBC","EBAYL","EDIT","ERI","CAPX","ECPG","ENOC","EGT","ESPR","EVK","EXFO","EZPW","FARO","FGEN","FITBI","BUSE","FFBC","FIBK","FPA","FVC","FTGC","FTC","FAD","CARZ","QTEC","FYC","FUNC","FLKS","FLXS","FORR","FRAN","FSBW","FSNN","GALTU","GENC","GNTX","GIGA","GOODP","GBLI","ACTX","ALTY","GOGL","GPIAW","GPP","GSIT","GURE","HBK","HRMNU","HWBK","HEBT","HTBK","HIHO","HFBL","HRZN","HTGM","HBANP","IBKC","ICUI","INFO","IMNP","IBCP","INNL","INSG","IART","LINK","ISIL","ISBC","DFVS","IRDMB","FALN","EWZS","QAT","IGOV","XXIA","JXSB","JAZZ","WYIG","KALU","KMPH","KGJI","KLREU","KLIC","LAMR","LE","LPTX","LMAT","LBTYA","BATRK","LCUT","LECO","LOB","CNCR","LMNX","MBVX","MNGA","LOAN","MRLN","MAT","MGRC","MDVX","MTSL","MMSI","MGEE","MVIS","MDXG","MITK","MNTA","MSDI","MSBF","MYOS","NSSC","NGHCN","NWLI","NCIT","NETE","NDRM","NEWS","EGOV","NSYS","NWFL","NUAN","NVDA","OBSV","ODP","OSBCP","ONSIZ","OPXAW","OSUR","OACQW","OTTW","PFIN","PPBI","PANL","PKOH","PAVMW","PDFS","PNNT","PRCP","PGLC","PICO","PLUG","PLKI","PSIX","PFI","PUI","KBWB","USLB","VRIG","PRGX","PSC","PRPH","PRTO","PULM","QGEN","QDEL","RADA","RAND","RDI","REPH","RNST","REFR","REXX","RVSB","ROSG","RUTH","SBRAP","SASR","SMIT","SHIP","SNFCA","SENEB","SREV","SHPG","SWIR","SLAB","SVA","SLMBP","SLRC","SPHS","SONA","DWFI","FUND","SBLKL","STLRU","BANX","SCMP","SPWR","SPRT","SYNL","SYPR","TNDM","AMTD","TELL","TSRA","TGTX","DXYN","FITS","NAVG","YORW","TSBK","TISA","TWMC","TRVN","TRNC","TTMI","TRCB","ULBI","UBCP","UBFO","UVSP","VALX","VIGI","VTWV","VBLT","TVIX","VRSN","VSAT","CSF","VKTX","VRTU","VYGR","WFBI","WCST","WBB","WHLRW","WLTW","AGND","GULF","WWD","WVFC","XGTIW","YIN","ZG","ZUMZ"]
-if loadTest:
-    L2 = []
-    for sym in L:
-        if len(sym) < 5 and sym[0] == "A":
-            print(sym)
-            L2.append(web.DataReader(sym, "yahoo", start, end))
-# Let's get Apple stock data; Apple's ticker symbol is AAPL
-# First argument is the series we want, second is the source ("yahoo" for Yahoo! Finance), third is the start date, fourth is the end date
-apple = web.DataReader("AAPL", "yahoo", start, end)
- 
-type(apple)
+if __name__ == "__main__":
+    loadTest = False
 
-#print(apple.head())
-#print("Size:\n", apple.count())
+    # We will look at stock prices over the past year, starting at January 1, 2016
+    start = datetime.datetime(2016,1,1)
+    end = datetime.date.today()
 
-numberDays = 10
+    L =["PIH","SHLM","ACTG","AKAO","ADMS","AAAP","AEHR","AGYS","ATSG","AMRI","ALJJ","ALQA","SWIN","AMBA","AETI","CRMT","IBUY","ANDAR","ABAC","AMAT","ARDM","APLP","ARTX","ASNA","ALOT","ACFC","ATRI","ADP","AHPAU","AZRX","BLDP","OZRK","BHACW","BBBY","BGCP","BLRX","BSTC","BBRY","BLBD","BNCN","BPFHP","BLIN","BRKS","CHRW","CLMS","CALA","CPLA","CAPN","CSII","CRTN","CBIO","CDW","CLLS","CSFL","CRNT","CFCOU","CHKP","CMRX","CNIT","IMOS","CMCT","CZFC","CLRO","CNV","CGNX","COLB","CHUBK","CGEN","CFMS","CFRX","CORT","COUP","CRAY","XRDC","CTIC","CVV","CONE","DRIOW","DWSN","DENN","DXTR","DRAD","DISH","DORM","DXPE","EWBC","EBAYL","EDIT","ERI","CAPX","ECPG","ENOC","EGT","ESPR","EVK","EXFO","EZPW","FARO","FGEN","FITBI","BUSE","FFBC","FIBK","FPA","FVC","FTGC","FTC","FAD","CARZ","QTEC","FYC","FUNC","FLKS","FLXS","FORR","FRAN","FSBW","FSNN","GALTU","GENC","GNTX","GIGA","GOODP","GBLI","ACTX","ALTY","GOGL","GPIAW","GPP","GSIT","GURE","HBK","HRMNU","HWBK","HEBT","HTBK","HIHO","HFBL","HRZN","HTGM","HBANP","IBKC","ICUI","INFO","IMNP","IBCP","INNL","INSG","IART","LINK","ISIL","ISBC","DFVS","IRDMB","FALN","EWZS","QAT","IGOV","XXIA","JXSB","JAZZ","WYIG","KALU","KMPH","KGJI","KLREU","KLIC","LAMR","LE","LPTX","LMAT","LBTYA","BATRK","LCUT","LECO","LOB","CNCR","LMNX","MBVX","MNGA","LOAN","MRLN","MAT","MGRC","MDVX","MTSL","MMSI","MGEE","MVIS","MDXG","MITK","MNTA","MSDI","MSBF","MYOS","NSSC","NGHCN","NWLI","NCIT","NETE","NDRM","NEWS","EGOV","NSYS","NWFL","NUAN","NVDA","OBSV","ODP","OSBCP","ONSIZ","OPXAW","OSUR","OACQW","OTTW","PFIN","PPBI","PANL","PKOH","PAVMW","PDFS","PNNT","PRCP","PGLC","PICO","PLUG","PLKI","PSIX","PFI","PUI","KBWB","USLB","VRIG","PRGX","PSC","PRPH","PRTO","PULM","QGEN","QDEL","RADA","RAND","RDI","REPH","RNST","REFR","REXX","RVSB","ROSG","RUTH","SBRAP","SASR","SMIT","SHIP","SNFCA","SENEB","SREV","SHPG","SWIR","SLAB","SVA","SLMBP","SLRC","SPHS","SONA","DWFI","FUND","SBLKL","STLRU","BANX","SCMP","SPWR","SPRT","SYNL","SYPR","TNDM","AMTD","TELL","TSRA","TGTX","DXYN","FITS","NAVG","YORW","TSBK","TISA","TWMC","TRVN","TRNC","TTMI","TRCB","ULBI","UBCP","UBFO","UVSP","VALX","VIGI","VTWV","VBLT","TVIX","VRSN","VSAT","CSF","VKTX","VRTU","VYGR","WFBI","WCST","WBB","WHLRW","WLTW","AGND","GULF","WWD","WVFC","XGTIW","YIN","ZG","ZUMZ"]
+    if loadTest:
+        L2 = []
+        for sym in L:
+            if len(sym) < 5 and sym[0] == "A":
+                print(sym)
+                L2.append(web.DataReader(sym, "yahoo", start, end))
+    # Let's get Apple stock data; Apple's ticker symbol is AAPL
+    # First argument is the series we want, second is the source ("yahoo" for Yahoo! Finance), third is the start date, fourth is the end date
+    apple = web.DataReader("AAPL", "yahoo", start, end)
 
-trainingData = np.random.random((1, numberDays * 6))
-trainingOutputs = np.random.random((1, 1))
+    type(apple)
 
-for i in range(numberDays):
-    trainingData[0][i * 6] = apple["Open"][i]
-    trainingData[0][i * 6 + 1] = apple["Close"][i]
-    trainingData[0][i * 6 + 2] = apple["High"][i]
-    trainingData[0][i * 6 + 3] = apple["Low"][i]
-    trainingData[0][i * 6 + 4] = apple["Volume"][i]
-    trainingData[0][i * 6 + 5] = apple["Adj Close"][i]
-i += 1
-trainingOutputs[0][0] = apple["Close"][i]
+    #print(apple.head())
+    #print("Size:\n", apple.count())
+
+    numberDays = 10
+
+    trainingData = np.random.random((1, numberDays * 6))
+    trainingOutputs = np.random.random((1, 1))
+
+    for i in range(numberDays):
+        trainingData[0][i * 6] = apple["Open"][i]
+        trainingData[0][i * 6 + 1] = apple["Close"][i]
+        trainingData[0][i * 6 + 2] = apple["High"][i]
+        trainingData[0][i * 6 + 3] = apple["Low"][i]
+        trainingData[0][i * 6 + 4] = apple["Volume"][i]
+        trainingData[0][i * 6 + 5] = apple["Adj Close"][i]
+    i += 1
+    trainingOutputs[0][0] = apple["Close"][i]
